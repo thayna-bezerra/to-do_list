6d483f3564d5bec4817276ca04a47ae06cd21cc7
thayna <arraisthayna2106@gmail.com>
2023-03-25 18:02:42 -0300

    refatorando interface

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './components/header';
+import { Tasks } from './components/tasks';
+
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Tasks/>
+    </div>
+  )
+}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,19 @@
 import styles from './Header.module.css'
 import logo from '../../assets/to-do.svg'
+import { PlusCircle } from 'phosphor-react';
 
 export function Header() {
   return (
     <header className={styles.header}>
       <img src={logo} alt="Logo to-do" />
+
+      <form className={styles.container}>
+        <input placeholder='Adicione uma nova tarefa' className={styles.input} />
+        <button className={styles.button}>
+          Criar
+          <PlusCircle size={20} weight="bold" />
+        </button>
+      </form>
     </header>
   );
 }
