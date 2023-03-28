0519bac8247ad01b2f7c38d60f9858ff75fe2428
thayna <arraisthayna2106@gmail.com>
2023-03-28 16:04:27 -0300

    showing warning screen when there are no tasks

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -12,27 +12,18 @@ interface Props {
 export function Task({ task, onDelete, onComplete }: Props) {
   return (
     <>
-      {Tasks.length <= 0 ? (
-        <div className={styles.emptyList}>
-          <hr />
-          <Notepad size={80} color="#333333" weight='light' />
-          <p><strong>Você ainda não tem tarefas cadastradas</strong> <br /> Crie tarefas e organize seus itens a fazer</p>
+      <div className={styles.task}>
+        <div className={styles.taskCheckbox}>
+          <input type="checkbox" onClick={() => onComplete(task.id)} />
         </div>
-      ) : (
-        <div className={styles.task}>
-          <div className={styles.taskCheckbox}>
-            <input type="checkbox" onClick={() => onComplete(task.id)} />
-          </div>
-          <div className={styles.taskContent}>
-            <p className={task.isCompleted ? styles.isChecked : ""}>{task.title}</p>
-          </div>
+        <div className={styles.taskContent}>
+          <p className={task.isCompleted ? styles.isChecked : ""}>{task.title}</p>
+        </div>
 
-          <button onClick={() => onDelete(task.id)}>
-            <Trash size={18} />
-          </button>
-        </div>
-      )
-      }
+        <button onClick={() => onDelete(task.id)}>
+          <Trash size={18} />
+        </button>
+      </div>
     </>
   )
 }
--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -1,3 +1,4 @@
+import { Notepad } from 'phosphor-react';
 import { TaskProps } from '../../App';
 import { Task } from '../task';
 import styles from './Tasks.module.css';
@@ -23,7 +24,14 @@ export function Tasks({ tasks, onDelete, onComplete }: Props ) {
           <Task key={task.id} task={task} onDelete={onDelete} onComplete={onComplete}/>
         ))}
       </div>
-      
+
+      {tasks.length <= 0 && (
+        <div className={styles.emptyList}>
+        <hr />
+        <Notepad size={80} color="#333333" weight='light' />
+        <p><strong>Você ainda não tem tarefas cadastradas</strong> <br /> Crie tarefas e organize seus itens a fazer</p>
+      </div>
+      )}
     </div>
   )
 }
